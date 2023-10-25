bb67ec7843b02600814901b5f6a6b73e92c7c9a2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-25 09:13:59 +0700

    add entity Customer

---
+++ customer-services/src/main/java/com/bayu/customer/model/Customer.java
@@ -0,0 +1,43 @@
+package com.bayu.customer.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "id_type")
+    private String idType;
+
+    @Column(name = "id_value")
+    private String idValue;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
+
+
+}
