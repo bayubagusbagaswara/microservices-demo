f1f3ecb318d218610542079e0d1bb0ec80976771
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-25 09:50:44 +0700

    add field createdAt

--- customer-services/src/main/java/com/bayu/customer/model/Customer.java
+++ customer-services/src/main/java/com/bayu/customer/model/Customer.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "customer")
 @Data
@@ -39,5 +41,7 @@ public class Customer {
     @Column(name = "phone_number", unique = true)
     private String phoneNumber;
 
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
 }
