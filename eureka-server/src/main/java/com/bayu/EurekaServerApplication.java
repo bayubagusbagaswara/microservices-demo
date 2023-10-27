4bb7f8331c5cae368e55db6302e9e876f1c140e8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-27 14:32:45 +0700

    update main application EurekaServerApplication

--- eureka-server/src/main/java/com/bayu/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/bayu/EurekaServerApplication.java
@@ -1,9 +1,20 @@
 package com.bayu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
+    private static final Logger log = Logger.getLogger(EurekaServerApplication.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("Eureka Server is running...");
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
