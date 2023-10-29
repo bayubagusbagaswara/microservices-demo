2c8536ef318426d4683d50d293635430595cb377
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-29 16:16:26 +0700

    inject fraud service in customer service

--- customer-services/src/main/java/com/bayu/customer/service/CustomerService.java
+++ customer-services/src/main/java/com/bayu/customer/service/CustomerService.java
@@ -1,11 +1,16 @@
 package com.bayu.customer.service;
 
+import com.bayu.clients.fraud.FraudCheckResponse;
+import com.bayu.clients.fraud.FraudRequest;
 import com.bayu.customer.dto.CustomerRegistrationRequest;
 import com.bayu.customer.model.Customer;
 import com.bayu.customer.repository.CustomerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +20,7 @@ import java.time.LocalDateTime;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest registrationRequest) {
         Customer customer = Customer.builder()
@@ -28,7 +34,38 @@ public class CustomerService {
                 .createdAt(LocalDateTime.now())
                 .build();
 
-        customerRepository.save(customer);
+        FraudRequest fraudRequest = new FraudRequest(
+                registrationRequest.idType(),
+                registrationRequest.idValue()
+        );
+
+        FraudCheckResponse fraudCheckResponse = new FraudCheckResponse();
+
+        // request checking customer fraud via RestTemplate
+        try {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            String fraudJsonRequest = objectMapper.writeValueAsString(fraudRequest);
+            HttpEntity<Object> httpEntity = new HttpEntity<>(fraudJsonRequest, httpHeaders);
+
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    "http://localhost:8081/api/v1/fraud-checks",
+                    HttpMethod.POST,
+                    httpEntity,
+                    String.class
+            );
+
+            fraudCheckResponse = objectMapper.readValue(responseEntity.getBody(), FraudCheckResponse.class);
+
+        } catch (Exception exception) {
+            log.error(exception.getLocalizedMessage());
+        }
+
+        if (null != fraudCheckResponse)
+
+        // customerRepository.save(customer);
         log.info("Customer info {}", customer);
     }
 
